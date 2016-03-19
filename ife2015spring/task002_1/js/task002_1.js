92e969c3456629ef6ded10200f32fcc7ef55f618
gloye <dbzbook@hotmail.com>
2016-03-19 12:09:58 +0800

    task002_1 stage3

--- ife2015spring/task002_1/js/task002_1.js
+++ ife2015spring/task002_1/js/task002_1.js
@@ -1,7 +1,6 @@
 /**
  * Created by admin on 2016/3/17.
  */
-
 //stage1
 /*function submit(event) {
  event.preventDefault();
@@ -18,14 +17,36 @@
  $("#result").innerHTML = uniqArray(arr) ;
  }*/
 //stage3
-function submit(event) {
-  event.preventDefault();
-  function writeError() {
-    var el = $("#error");
-    if (el) {
-      el.parentNode.removeChild(el);
-    }
-    $("#submit").insertAdjacentHTML('beforebegin', '<p id="error">请重新输入</p>');
+function readError(e) {
+  var val = e.target.value|| e.target.elements[0].value;
+  var reg = /\s|；|;|、|，/g;
+  var arr = val.replace(reg, ",").split(",");
+  if (val.length < 1) {
+    writeError("不能为空");
+  } else if (arr.length > 10) {
+    writeError("数目不能超过10");
+  } else {
+    clearError();
+    $("#result").innerHTML = uniqArray(arr);
   }
+};
+function clearError() {
+  var el = $("#error");
+  if (el) {
+    el.parentNode.removeChild(el);
+  }
+};
+function writeError(msg) {
+  clearError();
+  $("#submit").insertAdjacentHTML('beforebegin', '<p id="error">' + msg + '</p>');
+};
+function submitForm(event) {
+  event.preventDefault();
+  var btn = event.target.elements['submit'];
+  btn.disabled = true;
+  readError(event);
+  var input = event.target.elements[0];
+  $.on(input, "change", readError);
 }
-$.on("#form", "submit", submit);
+//$.on("#input", "change", readError);
+$.on("#form", "submit", submitForm);
