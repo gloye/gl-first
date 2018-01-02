c1a77a14b9e4aae31926c972ec930c69d738ae7c
Gloye <gloye.sun@dookay.com>
2018-01-02 15:44:44 +0800

    更新文件

--- my_practice/countDown/index.js
+++ my_practice/countDown/index.js
@@ -13,10 +13,40 @@ function countDownSeconds(seconds) {
     }, 1000)
 }
 
-function counntDownDate(date) {
-    const dateTime = new Date(date).getTime();
-
+function countDownDate() {
+    const dist = '2018-01-03'
+    const end = new Date(dist).getTime()
+    const start = new Date().getTime()
+    const res = parseInt((end - start) / 1000)
+    return res
 }
+
+function format(art) {
+    const src = art
+    const unit = {
+        minute: 60,
+        hour: 60 * 60,
+        day: 60 * 60 * 24
+    }
+    const resDay = Math.ceil(src / (unit.day))
+    const resHour = Math.ceil(src % (unit.day) / (unit.hour))
+    const resMin = Math.ceil(src % (unit.hour) / (unit.minute))
+    const resSec = Math.ceil(src % (unit.minute))
+
+    const res = resDay + ':' + resHour + ':' + resMin + ':' + resSec
+    console.log(res)
+
+    if (src > 0) {
+        setTimeout(
+            function () {
+                format(src - 1)
+            } , 1000
+        )
+    }
+}
+format(countDownDate())
+
+
 
 function countDownMinutes(minutes) {
 
