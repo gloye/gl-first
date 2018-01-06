73fd81603d24d7ca2c4b168b1fd0604e10854496
gloye <dbzbook@hotmail.com>
2018-01-06 18:07:00 +0800

    更新文件

--- my_practice/python/demo.py
+++ my_practice/python/demo.py
@@ -1,14 +1,38 @@
 '''
 # demo
 '''
-import requests
-import sys
-import webbrowser
-import bs4
 import os
+import requests
+import bs4
 
-print('searching...')
 
-url = "http://manhua.dmzj.com/longzhuchao"
+URL = "http://manhua.dmzj.com/longzhuchao/72151.shtml#@page=1"
+
+print('Downloading page %s...' % URL)
 
 os.makedirs('dragonball', exist_ok=True)
+
+RES = requests.get(URL)
+RES.raise_for_status()
+
+SOUP = bs4.BeautifulSoup(RES.text, 'html5lib')
+
+
+IMG_EL = SOUP.select('#center_box')
+print(IMG_EL)
+exit()
+comicUrl = False
+if IMG_EL == []:
+    print('Could not find comic image.')
+else:
+    comicUrl = IMG_EL[0].get('src')
+    print('Downloading image %s...' % (comicUrl))
+    res = requests.get(comicUrl)
+    res.raise_for_status()
+
+if comicUrl:
+    imageFile = open(os.path.join(
+        'dragonball', os.path.basename(comicUrl)), 'wb')
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+        imageFile.close()
