aa2fa283979780fd68e541152ef61e29293df5cd
gloye <dbzbook@hotmail.com>
2018-01-13 21:16:50 +0800

    添加一个小球

--- my_practice/Krakout/js/main.js
+++ my_practice/Krakout/js/main.js
@@ -13,10 +13,56 @@ const Paddle = function () {
     speed: 20
   }
   o.moveLeft = function () {
+    if (o.xAxis <= 0) {
+      o.xAxis = 0
+      return
+    }
     o.xAxis -= o.speed
   }
   o.moveRight = function () {
+    if (o.xAxis >= 800) {
+      o.xAxis = 800
+      return
+    }
     o.xAxis += o.speed
+  }
+  o.collide = function (ball) {
+    if (ball.yAxis + ball.image.height > o.yAxis) {
+      if (ball.xAxis > o.xAxis && ball.x < o.xAxis + o.image.width) {
+        log('相撞')
+        return true
+      }
+    }
+    return false
+  }
+  return o
+}
+
+const Ball = function () {
+  const image = imageFromPath('../images/ball.png')
+  const o = {
+    image: image,
+    xAxis: 0,
+    yAxis: 20,
+    speedX: 10,
+    speedY: 10,
+    fired: false
+  }
+  o.fire = function () {
+    o.fired = true
+  }
+  o.move = function () {
+    if (o.fired) {
+      if (o.xAxis < 0 || o.xAxis > 1000) {
+        o.speedX = -o.speedX
+      }
+      if (o.yAxis < 0 || o.yAxis > 500) {
+        o.speedY = -o.speedY
+      }
+      // move
+      o.xAxis += o.speedX
+      o.yAxis += o.speedY
+    }
   }
   return o
 }
@@ -52,6 +98,7 @@ const Game = function () {
         g.actions[key]()
       }
     }
+    g.update()
     g.clear()
     g.draw()
   }, 1000 / 30)
@@ -75,6 +122,7 @@ const Game = function () {
 const log = console.log.bind(console)
 const __main = function () {
   const paddle = Paddle()
+  const ball = Ball()
   const game = Game()
   game.registerAction('a', function () {
     paddle.moveLeft()
@@ -82,8 +130,20 @@ const __main = function () {
   game.registerAction('d', function () {
     paddle.moveRight()
   })
+  game.registerAction('f', function () {
+    ball.fire()
+  })
+  game.update = function () {
+    ball.move()
+    // 判断相撞
+    if (paddle.collide(ball)) {
+      // 这里应该调用一个 ball.反弹() 来实现
+      ball.speedY *= -1
+    }
+  }
   game.draw = function () {
     game.drawImage(paddle)
+    game.drawImage(ball)
   }
 }
 __main()
